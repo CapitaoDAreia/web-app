7237516bfadcf68951c908b5fb08f7fe83e27dd0
CapitaoDAreia <danieligord7@hotmail.com>
2023-02-17 15:49:53 -0300

    Setting up main function.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,27 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 type application struct{}
 
 func main() {
 	// Set up an app config
+	app := application{}
 
 	//get app routes
+	mux := app.routes()
 
 	//print out a message
-
+	log.Println("Server is listening on port 8080")
 	// start the server
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
 }
+
+//manhã
+//- explorando o painel para mapear as batidas na store-api e usar um client para efetuar esse tipo de operação
+//- acompanhamos o Vini subindo a task de validação do controller de store-configurations da store-api --------- e aplicando um fix no way-to-go
