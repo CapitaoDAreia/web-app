f6a899764d43bc197541932593d7c4fb792d2d9b
CapitaoDAreia <danieligord7@hotmail.com>
2023-02-18 19:04:38 -0300

    Ending webapp and preparing to testing.

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -1,10 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Home")
+	_ = app.render(w, r, "home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(w http.ResponseWriter, r *http.Request, t string, data interface{}) error {
+	parsedTemplate, err := template.ParseFiles("./templates/" + t)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	err = parsedTemplate.Execute(w, data)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return err
+	}
+	return nil
 }
