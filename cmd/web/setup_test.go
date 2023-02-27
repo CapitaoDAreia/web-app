ecf8686aa757bdfeee7ad580b8d5eec3644b9732
CapitaoDAreia <danieligord7@hotmail.com>
2023-02-27 20:32:43 -0300

    Creating a testdb repository.

--- cmd/web/setup_test.go
+++ cmd/web/setup_test.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"log"
 	"os"
 	"testing"
-	"web-app/pkg/db"
+	"web-app/pkg/repository/dbrepo"
 )
 
 var app application
@@ -13,22 +12,8 @@ func TestMain(m *testing.M) {
 	pathToTemplates = "./../../templates/"
 
 	app.Session = getSession()
-	app.DataSourceName = `
-	host=localhost 
-	port=5432 
-	user=postgres 
-	password=postgres 
-	dbname=users 
-	timezone=UTC 
-	connect_timeout=5`
 
-	connection, err := app.connectToDB()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer connection.Close()
-
-	app.DB = &db.PostgresConn{DB: connection}
+	app.DB = &dbrepo.TestDBREpo{}
 
 	//Execute before tests run
 	os.Exit(m.Run())
---
+++ pkg/repository/dbrepo/users_testdb.go
@@ -0,0 +1,63 @@
+package dbrepo
+
+import (
+	"database/sql"
+	"web-app/pkg/data"
+)
+
+type TestDBREpo struct {
+}
+
+func (m *TestDBREpo) Connection() *sql.DB {
+	return nil
+}
+
+// AllUsers returns all users as a slice of *data.User
+func (m *TestDBREpo) AllUsers() ([]*data.User, error) {
+	var users []*data.User
+
+	return users, nil
+}
+
+// GetUser returns one user by id
+func (m *TestDBREpo) GetUser(id int) (*data.User, error) {
+	var user = data.User{
+		ID: 1,
+	}
+
+	return &user, nil
+}
+
+// GetUserByEmail returns one user by email address
+func (m *TestDBREpo) GetUserByEmail(email string) (*data.User, error) {
+	var user = data.User{
+		ID: 1,
+	}
+
+	return &user, nil
+}
+
+// UpdateUser updates one user in the database
+func (m *TestDBREpo) UpdateUser(u data.User) error {
+	return nil
+}
+
+// DeleteUser deletes one user from the database, by id
+func (m *TestDBREpo) DeleteUser(id int) error {
+	return nil
+}
+
+// InsertUser inserts a new user into the database, and returns the ID of the newly inserted row
+func (m *TestDBREpo) InsertUser(user data.User) (int, error) {
+	return 2, nil
+}
+
+// ResetPassword is the method we will use to change a user's password.
+func (m *TestDBREpo) ResetPassword(id int, password string) error {
+	return nil
+}
+
+// InsertUserImage inserts a user profile image into the database.
+func (m *TestDBREpo) InsertUserImage(i data.UserImage) (int, error) {
+	return 1, nil
+}
