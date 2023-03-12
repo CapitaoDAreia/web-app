3f9458f7c8aef676fcf16485ed602cd3fd6387d7
CapitaoDAreia <danieligord7@hotmail.com>
2023-03-11 22:58:06 -0300

    Testing refreshing tokens

--- cmd/api/api-handlers_test.go
+++ cmd/api/api-handlers_test.go
@@ -4,8 +4,11 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"strings"
 	"testing"
+	"time"
+	"web-app/pkg/data"
 )
 
 func TestAppAuthenticate(t *testing.T) {
@@ -71,4 +74,72 @@ func TestAppAuthenticate(t *testing.T) {
 			t.Errorf("%s: returned wrong status code; expected %d but got %d", test.name, test.expectedStatusCode, rr.Code)
 		}
 	}
+}
+
+func TestAppRefresh(t *testing.T) {
+	var tests = []struct {
+		name               string
+		token              string
+		expectedStatusCode int
+		resetRefreshTime   bool
+	}{
+		{
+			name:               "Valid",
+			token:              "",
+			expectedStatusCode: http.StatusOK,
+			resetRefreshTime:   true,
+		},
+		{
+			name:               "Valid but not yet ready to expire",
+			token:              "",
+			expectedStatusCode: http.StatusTooEarly,
+			resetRefreshTime:   false,
+		},
+		{
+			name:               "Expired token",
+			token:              expiredToken,
+			expectedStatusCode: http.StatusBadRequest,
+			resetRefreshTime:   false,
+		},
+	}
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	oldRefreshTime := refreshTokenExpiry
+
+	for _, test := range tests {
+		var tkn string
+		if test.token == "" {
+			if test.resetRefreshTime {
+				refreshTokenExpiry = time.Second * 1
+			}
+			tokens, _ := app.generateTokenPair(&testUser)
+			tkn = tokens.RefreshToken
+		} else {
+			tkn = test.token
+		}
+
+		postedData := url.Values{
+			"refresh_token": {tkn},
+		}
+
+		req, _ := http.NewRequest("POST", "/refresh-token", strings.NewReader(postedData.Encode()))
+		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+		rr := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(app.refresh)
+		handler.ServeHTTP(rr, req)
+
+		if rr.Code != test.expectedStatusCode {
+			t.Errorf("%s: expected status of %d but got %d", test.name, test.expectedStatusCode, rr.Code)
+		}
+
+		refreshTokenExpiry = oldRefreshTime
+	}
+
 }
