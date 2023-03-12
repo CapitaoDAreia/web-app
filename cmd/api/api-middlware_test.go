0089d2ed8709cdadf0fbdc6fcaa0d6ae2fc2b4f8
CapitaoDAreia <danieligord7@hotmail.com>
2023-03-11 22:00:35 -0300

    Testing authRequired middleware

--- cmd/api/api-middlware_test.go
+++ cmd/api/api-middlware_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"web-app/pkg/data"
 )
 
 func TestAppEnableCORS(t *testing.T) {
@@ -42,6 +44,66 @@ func TestAppEnableCORS(t *testing.T) {
 
 		if !test.expectedHeader && rr.Header().Get("Access-Control-Allow-Credentials") != "" {
 			t.Errorf("%s: expected no header, but got one", test.name)
+		}
+	}
+}
+
+func TestAppAuthRequired(t *testing.T) {
+	nextHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {})
+
+	testUser := data.User{
+		ID:        1,
+		FirstName: "Admin",
+		LastName:  "User",
+		Email:     "admin@example.com",
+	}
+
+	tokens, _ := app.generateTokenPair(&testUser)
+
+	var tests = []struct {
+		name       string
+		token      string
+		expectAuth bool
+		setHeader  bool
+	}{
+		{
+			name:       "Valid token",
+			token:      fmt.Sprintf("Bearer %s", tokens.Token),
+			expectAuth: true,
+			setHeader:  true,
+		},
+		{
+			name:       "No token",
+			token:      "",
+			expectAuth: false,
+			setHeader:  false,
+		},
+		{
+			name:       "Invalid token",
+			token:      fmt.Sprintf("Bearer %s", expiredToken),
+			expectAuth: false,
+			setHeader:  true,
+		},
+	}
+
+	for _, test := range tests {
+		req, _ := http.NewRequest("GET", "/", nil)
+		if test.setHeader {
+			req.Header.Set("Authorization", test.token)
+		}
+
+		rr := httptest.NewRecorder()
+
+		handlerToTest := app.authRequired(nextHandler)
+
+		handlerToTest.ServeHTTP(rr, req)
+
+		if test.expectAuth && rr.Code == http.StatusUnauthorized {
+			t.Errorf("%s: got code for 401, and should not have", test.name)
+		}
+
+		if !test.expectAuth && rr.Code != http.StatusUnauthorized {
+			t.Errorf("%s: did not get code 401, and should have", test.name)
 		}
 	}
 }
